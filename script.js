3d604f08384359d91746b6124c1f742581446be7
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-03-03 11:11:53 -0700

    addd clear function to textarea

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let correctAnswer = "red";
 
 document.getElementById("button").addEventListener("click", checkGuess);
+document.getElementById("button").addEventListener("click", eraseContents);
 
 function checkGuess() {
   let guess = document.getElementById("the_guess").value;
@@ -10,6 +11,10 @@ function checkGuess() {
     alert("Correct! Great Job!")
   } else {
     alert("Wrong-o!")
+  }
+
+  function eraseContents () {
+    document.getElementById("the_guess").value = ""
   }
 }
 
