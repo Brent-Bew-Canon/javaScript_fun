ca19654e1141f80216a58792df6bd960817b4e0e
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-03-03 11:38:54 -0700

    updated eventlistener

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 let correctAnswer = "red";
 
 document.getElementById("button").addEventListener("click", checkGuess);
-document.getElementById("button").addEventListener("click", eraseContents);
+
 
 function checkGuess() {
   let guess = document.getElementById("the_guess").value;
-    guess = guess.toLowerCase();
-    console.log(guess);
+  guess = guess.toLowerCase();
+  console.log(guess);
+  document.getElementById("the_guess").value = "";
   if (guess == correctAnswer) {
     alert("Correct! Great Job!")
   } else {
     alert("Wrong-o!")
   }
-
-  function eraseContents () {
-    document.getElementById("the_guess").value = ""
-  }
 }
-
